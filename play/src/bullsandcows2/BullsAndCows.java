bfd398389f545db03d840cacfb1b749619b7f320
Serei63 <runesiel@gmail.com>
2021-04-04 22:13:17 +0900

    숫자야구 구현 2

---
+++ play/src/bullsandcows2/BullsAndCows.java
@@ -0,0 +1,36 @@
+package bullsandcows2;
+
+public class BullsAndCows {
+	private int[] target = new int[4];
+	private String gt;
+	private int strike;
+	private int ball;
+	private int out;
+		
+	public void setTarget() {
+		target[0] = (int) (Math.random() * 10);
+
+		do {
+			target[1] = (int) (Math.random() * 10);
+		} while (target[1] == target[0]);
+
+		do {
+			target[2] = (int) (Math.random() * 10);
+		} while (target[2] == target[0] || target[2] == target[1]);
+
+		do {
+			target[3] = (int) (Math.random() * 10);
+		} while (target[3] == target[0] || target[3] == target[1] || target[3] == target[2]);
+	}
+
+	public String getTarget() {
+		for (int i = 0; i < target.length; i++) {
+			gt = target[i] + "";
+			}
+		return gt;
+	}
+
+	public void checkNum(String checkNum) {
+		
+	}
+}
---
+++ play/src/bullsandcows2/BullsAndCowsRun.java
@@ -0,0 +1,18 @@
+package bullsandcows2;
+
+public class BullsAndCowsRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 설명칸
+		System.out.println("[숫자야구]");
+		System.out.println("각 자리가 0부터 9까지인 4자리 숫자를 맞추는 게임입니다.");
+		System.out.println("자리와 숫자가 모두 맞으면 스트라이크 (S)");
+		System.out.println("숫자는 있으나 자리가 맞지 않으면 볼 (B)");
+		System.out.println("없는 숫자가 입력되었을 경우 아웃(O)의 카운트가 올라갑니다.");
+		System.out.println("---------------------------------------------------------------------");
+		// 여기까지
+
+	}
+
+}
